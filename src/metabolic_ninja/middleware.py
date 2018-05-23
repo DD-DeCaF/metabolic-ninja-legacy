44cc0cab511a858eef0090d9895d055822672da0
Ali Kaafarani <ali@kvikshaug.no>
2018-05-23 13:58:46 +0200

    fix: import

--- src/metabolic_ninja/middleware.py
+++ src/metabolic_ninja/middleware.py
@@ -12,8 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from settings import raven_client
 import logging
+
+from . import raven_client
 
 
 logger = logging.getLogger(__name__)
